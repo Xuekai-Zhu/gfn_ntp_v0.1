d4a2c39354cbe114ea68a6bc23b9bce3b8644265
xuekaizhu_linlab <xuekaizhu0@gmail.com>
2024-12-12 20:29:06 +0800

    update

--- test_eof_error.py
+++ test_eof_error.py
@@ -8,3 +8,4 @@ from proof_flow.src.utils import prepare_environment_for_lean_dojo, repo_root
 local_repo_path = os.path.expanduser(
     "/scratch2/nlp/zhuxuekai/reasoning_flow/baselines/.cache/lean_dojo/leanprover-community-mathlib4-29dcec074de168ac2bf835a77ef68bbe069194c5/mathlib4")
 repo = LeanGitRepo.from_path(local_repo_path)
+print(repo)
