9d74b9d1f5e60c68c3b52171537a9a1dfb9b6ee1
xuekaizhu_linlab <xuekaizhu0@gmail.com>
2024-12-13 13:38:50 +0800

    update

--- test_eof_error_new.py
+++ test_eof_error_new.py
@@ -15,24 +15,11 @@ from proof_flow.src.utils import prepare_environment_for_lean_dojo, repo_root
 
 prepare_environment_for_lean_dojo()
 
-#command: /home/zhuxuekai/.elan/bin/lake
-# args: [b'/home/zhuxuekai/.elan/bin/lake', b'env', b'lean', b'--threads=1', b'--memory=32768', b'Mathlib/Data/Relqk2dq38z.lean']
-#
-
-# cached_path = os.path.expanduser(
-    # "/scratch2/nlp/zhuxuekai/reasoning_flow/baselines/.cache/lean_dojo/leanprover-community-mathlib4-29dcec074de168ac2bf835a77ef68bbe069194c5/mathlib4")
-# repo = LeanGitRepo.from_path(cached_path)
-
-
-build_deps = True
-
-traced_repo = TracedRepo.load_from_disk(cached_path, build_deps)
-traced_repo.check_sanity()
-
-print("traced_files_graph", traced_repo.traced_files_graph)
-print("traced_files", len(traced_repo.traced_files))
-
-print("-----------------------")
+repo = LeanGitRepo(
+    "https://github.com/leanprover-community/mathlib4",
+    "29dcec074de168ac2bf835a77ef68bbe069194c5",
+)
+traced_repo = trace(repo)
 
 # traced_file = traced_repo.get_traced_file("Mathlib/Data/Relqk2dq38z.lean")
 traced_file = traced_repo.get_traced_file("Mathlib/Algebra/ContinuedFractions/Translations2qx3rbck.lean")
